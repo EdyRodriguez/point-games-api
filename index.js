b273679e7d4472fd43966c0f265c3f9a8be42f8c
Edy Rodriguez <zeronoaz@gmail.com>
2023-12-25 15:35:10 -0600

    simple api

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const admin = require("firebase-admin");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://twitch-point-games.firebaseio.com",
+});
+
+const app = express();
+const db = admin.firestore();
+
+// Define an endpoint to get game keys
+app.get("/getGames", async (req, res) => {
+  try {
+    const keysSnapshot = await db.collection("juegos").get();
+    const keys = keysSnapshot.docs.map((doc) => {
+      const data = doc.data();
+      // Exclude the "key" field from the response
+      const { key, ...rest } = data;
+      return rest;
+    });
+    res.json(keys);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.use(bodyParser.json());
+
+app.post("/importGames", async (req, res) => {
+  try {
+    const games = req.body;
+
+    if (!Array.isArray(games)) {
+      return res.status(400).json({ error: "Invalid request body" });
+    }
+
+    const gamesData = games.map((gameName) => ({
+      canjeado: false,
+      key: "",
+      nombre: gameName,
+      usuario: "",
+    }));
+
+    const batch = db.batch();
+    gamesData.forEach((game) => {
+      const newGameRef = db.collection("juegos").doc(game.nombre);
+      batch.set(newGameRef, game);
+    });
+
+    await batch.commit();
+
+    res.json({ success: true, message: "Games imported successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
