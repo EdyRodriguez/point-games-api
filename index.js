3c0b650e6de3df6ac36c080304174d30b53c510d
Edy Rodriguez <zeronoaz@gmail.com>
2023-12-25 19:25:43 -0600

    fixed error with cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const admin = require("firebase-admin");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 require("dotenv").config();
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
