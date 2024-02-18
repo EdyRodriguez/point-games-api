3888175be5aa0e9fa3dda85008df51e4af2b1456
Edy Rodriguez <zeronoaz@gmail.com>
2024-02-18 13:46:58 -0600

    hotfix: rename api secret key

--- middlewareApi.js
+++ middlewareApi.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const validacionApiKey = (req, res, next) => {
   const apiKey = req.headers.apikey;
-  if (apiKey !== process.env.API_Secret_Key) {
+  if (apiKey !== process.env.API_SECRET_KEY) {
     res.status(401).json({ error: "ApiKey invalid" });
   }else{
   next();
